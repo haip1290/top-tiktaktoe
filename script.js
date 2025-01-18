e517f9409a43fa857fd93725ac4cd079d20f161b
Hai Pham <thanh_hai1290@yahoo.com>
2025-01-18 20:03:13 +0700

    Add gameController module and displayController module
    gameController module return makeMove method and getPlayer getter. It has turn, player, board as properties. It also has method to checkWinner and checkDraw.
    displayController module has makeMove for players, showWinner, showDraw, showPlayer to display player and game result on the site. It also run disableBoard method after game is done.

--- script.js
+++ script.js
@@ -1,16 +1,113 @@
-const gameBoard = (function () {
-  let gameBoard = [
-    [, ,],
-    [, ,],
-    [, ,],
+const gameController = (function () {
+  let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
-  const place = function (symbol, row, col) {
-    this.gameBoard[row][col] = symbol;
-  };
-  return { gameBoard, place };
+  const boardSize = 3;
+  let turn = 1;
+  let player = "X";
+
+  function makeMove(row, col) {
+    if (row >= boardSize || row < 0 || col >= boardSize || col < 0) {
+      console.error("Invalid move, cell must be on 3x3 board");
+      return;
+    }
+    if (board[row][col] !== "") {
+      console.error("Invalid move, cell already taken");
+      return;
+    }
+    board[row][col] = player;
+
+    if (checkWinner()) {
+      return player;
+    }
+    if (checkDraw()) {
+      return "draw";
+    }
+    player = player === "X" ? "O" : "X";
+    turn++;
+    return false;
+  }
+
+  function checkWinner() {
+    for (let i = 0; i < boardSize; i++) {
+      // check rows
+      symbol = board[i][0];
+      if (symbol !== "" && board[i][1] === symbol && board[i][2] === symbol) {
+        return true;
+      }
+      // check columns
+      symbol = board[0][i];
+      if (symbol !== "" && board[1][i] === symbol && board[2][i] === symbol) {
+        return true;
+      }
+    }
+    // check diag
+    symbol = board[1][1];
+    if (symbol !== "" && board[0][0] === symbol && board[2][2] === symbol) {
+      return true;
+    }
+    if (symbol !== "" && board[0][2] === symbol && board[2][0] === symbol) {
+      return true;
+    }
+    return false;
+  }
+
+  function checkDraw() {
+    if (turn === 9 && !checkWinner()) {
+      return true;
+    }
+    return false;
+  }
+
+  function getPlayer() {
+    return player;
+  }
+  return { getPlayer, makeMove };
 })();
 
-const player = (function () {})();
+const displayController = (function () {
+  showPlayer();
+  const cells = document.querySelectorAll(".cell");
+  for (const cell of cells) {
+    cell.addEventListener("click", makeMove);
+  }
 
-gameBoard.place("X", 0, 0);
-console.log(gameBoard);
+  function makeMove(event) {
+    const cell = event.target;
+    cell.textContent = gameController.getPlayer();
+    let cellId = cell.id.split("");
+    let result = gameController.makeMove(cellId[0], cellId[1]);
+    if (result === "X" || result === "O") {
+      showWinner(result);
+    }
+    if (result === "draw") {
+      showDraw();
+    }
+    showPlayer();
+  }
+
+  function showPlayer() {
+    const display = document.querySelector(".player-display");
+    display.textContent = gameController.getPlayer();
+  }
+
+  function showWinner(winner) {
+    const display = document.querySelector(".winner-display");
+    display.textContent = `${winner} won`;
+    disableBoard();
+    return;
+  }
+  function showDraw() {
+    const display = document.querySelector(".winner-display");
+    display.textContent = `Draw`;
+    disableBoard();
+  }
+
+  function disableBoard() {
+    for (const cell of cells) {
+      cell.removeEventListener("click", makeMove);
+    }
+  }
+})();
